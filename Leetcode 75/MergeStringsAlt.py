1c93b694e25aa6182cea6b5092f8ba0e5f56edeb
Aztarok <Aztarok2994@gmail.com>
2024-03-19 00:51:52 -0400

    Leetcode 1768 Done!

---
+++ Leetcode 75/MergeStringsAlt.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        i = 0
+        for c1, c2 in zip(word1, word2):
+            result.append(c1 + c2)
+            i += 1
+        result.extend(word1[i:] or word2[i:])
+        return "".join(result)
+
+
+def main() -> None:
+    s = "abcd"
+    t = "pq"
+    solution = Solution()
+    result = solution.mergeAlternately(s, t)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
