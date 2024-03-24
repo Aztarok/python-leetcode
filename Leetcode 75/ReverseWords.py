36bf175148b287a348000a7c8afb5e710710eab1
Aztarok <Aztarok2994@gmail.com>
2024-03-23 23:20:07 -0400

    Leetcode 151 Done!

---
+++ Leetcode 75/ReverseWords.py
@@ -0,0 +1,19 @@
+import re
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = re.split(r"\s+", s.strip())
+        words.reverse()
+        return " ".join(words)
+
+
+def main() -> None:
+    s = "  hello world  "
+    solution = Solution()
+    result = solution.reverseWords(s)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
