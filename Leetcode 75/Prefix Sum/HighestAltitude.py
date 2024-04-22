510e279beba1e1d651bd978263f4dc4bc399ecd6
Aztarok <Aztarok2994@gmail.com>
2024-04-22 15:04:58 -0400

    Leetcode 1732 Done!

---
+++ Leetcode 75/Prefix Sum/HighestAltitude.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        max_altitude = 0
+        altitude = 0
+        for i in gain:
+            altitude += i
+            max_altitude = max(max_altitude, altitude)
+        return max_altitude
+
+
+def main() -> None:
+    gain = [-5, 1, 5, 0, -7]
+    solution = Solution()
+
+    result = solution.largestAltitude(gain)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
